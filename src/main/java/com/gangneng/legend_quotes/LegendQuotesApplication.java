202a4d6a56fe4bfa86816e7f40a379b5de2a1616
itzelic-code <en3873@gmail.com>
2025-07-24 19:06:09 +0900

    Initial commit

---
+++ src/main/java/com/gangneng/legend_quotes/LegendQuotesApplication.java
@@ -0,0 +1,13 @@
+package com.gangneng.legend_quotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegendQuotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegendQuotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gangneng/legend_quotes/LegendQuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gangneng.legend_quotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegendQuotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
