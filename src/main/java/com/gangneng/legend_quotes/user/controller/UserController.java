06600481cd6c26b4021ac585f768175ef560bf8f
itzelic-code <en3873@gmail.com>
2025-08-19 21:17:48 +0900

    [#1] 회원가입 기능 개발

---
+++ src/main/java/com/gangneng/legend_quotes/user/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.gangneng.legend_quotes.user.controller;
+
+import com.gangneng.legend_quotes.user.dto.request.UserSignUpRequestDTO;
+import com.gangneng.legend_quotes.user.dto.response.UserSignUpResponseDTO;
+import com.gangneng.legend_quotes.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<UserSignUpResponseDTO> signUp(@RequestBody UserSignUpRequestDTO requestDTO) {
+        UserSignUpResponseDTO responseDTO = userService.signUp(requestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
+    }
+}
